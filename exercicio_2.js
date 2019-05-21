71b0e6be8b7d861afb0511fd6c0bc7b62cd29003
brunoknop <brunog.knop@gmail.com>
2019-05-21 01:55:29 -0300

    recuperação exercício 2 e tentativa em js

--- exercicio_2.js
+++ exercicio_2.js
@@ -1,15 +1,28 @@
-function constroiPiramide(tamanhoPiramide) {
-    for (var quantidaDeColunas = 1; quantidaDeColunas <= tamanhoPiramide; quantidaDeColunas++) {
-        for (var quantidadeDeLinhas = 0; quantidadeDeLinhas < quantidaDeColunas; quantidadeDeLinhas++) {
-            document.write('*');   
-        }
-        document.write('<br>');
+// function constroiPiramide(tamanhoPiramide) {
+//     for (var quantidaDeColunas = 1; quantidaDeColunas <= tamanhoPiramide; quantidaDeColunas++) {
+//         for (var quantidadeDeLinhas = 0; quantidadeDeLinhas < quantidaDeColunas; quantidadeDeLinhas++) {
+//             console.log('*');   
+//         }
+//         console.log('x');
+//     }
+//     for (var quantidaDeColunas = tamanhoPiramide-1; quantidaDeColunas > 0; quantidaDeColunas=quantidaDeColunas-1) {
+//         for (var quantidadeDeLinhas = 0; quantidadeDeLinhas < quantidaDeColunas; quantidadeDeLinhas++) {
+//             console.log('*');   
+//         }
+//         console.log(<br></br>);
+//     }
+// }
+function constroiPiramide(tamanho) {
+    //let asterisco = [['*'],['*','*'],['*','*','*'],['*','*','*','*'],['*','*','*','*','*']];
+    let asterisco = [];
+    for(let contArray = 0; contArray < tamanho; contArray++) {
+        asterisco.push(['*']);
+        // for(let contAst = 0; contAst < contArray; contAst++){
+        // }
     }
-    for (var quantidaDeColunas = tamanhoPiramide-1; quantidaDeColunas > 0; quantidaDeColunas=quantidaDeColunas-1) {
-        for (var quantidadeDeLinhas = 0; quantidadeDeLinhas < quantidaDeColunas; quantidadeDeLinhas++) {
-            document.write('*');   
-        }
-        document.write('<br>');
-    }
+console.log(asterisco)
+    // for (let contadorIda = 0; contadorIda < tamanho; contadorIda++) {
+    //      console.dir(asterisco[contadorIda].join(''));
+    // }
 }
 constroiPiramide(5);
